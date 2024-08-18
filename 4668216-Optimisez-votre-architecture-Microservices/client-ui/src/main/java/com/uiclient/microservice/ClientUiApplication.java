7ce2b998c730bf02b3aaea3d8433f7e5394f32c1
patrick valeri <patrick.valeri@free.fr>
2024-08-18 11:57:05 +0200

    Commit Etape 4 - Seul souci est affichage en colonne des produits

--- 4668216-Optimisez-votre-architecture-Microservices/client-ui/src/main/java/com/uiclient/microservice/ClientUiApplication.java
+++ 4668216-Optimisez-votre-architecture-Microservices/client-ui/src/main/java/com/uiclient/microservice/ClientUiApplication.java
@@ -2,12 +2,13 @@ package com.uiclient.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.uiclient")
-//@RibbonClient
+@EnableDiscoveryClient
 public class ClientUiApplication {
 
 	public static void main(String[] args) {
--- 4668216-Optimisez-votre-architecture-Microservices/microservice-commandes/src/main/java/com/mcommandes/McommandesApplication.java
+++ 4668216-Optimisez-votre-architecture-Microservices/microservice-commandes/src/main/java/com/mcommandes/McommandesApplication.java
@@ -3,8 +3,10 @@ package com.mcommandes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.mcommandes")
 @EnableDiscoveryClient
 public class McommandesApplication {
 
