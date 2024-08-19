6b7adb7a335e08664e3803c40a0c71e8ffe49116
patrick valeri <patrick.valeri@free.fr>
2024-08-19 08:30:47 +0200

    Commit avec Zuul sans filtre mais http 9004 fonctionne pas

--- 4668216-Optimisez-votre-architecture-Microservices/zuul-server/src/main/java/com/micrommerce/zuul_server/ZuulServerApplication.java
+++ 4668216-Optimisez-votre-architecture-Microservices/zuul-server/src/main/java/com/micrommerce/zuul_server/ZuulServerApplication.java
@@ -1,17 +1,17 @@
-package com.uiclient.microservice;
+package com.micrommerce.zuul_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableFeignClients("com.uiclient")
+@EnableZuulProxy
 @EnableDiscoveryClient
-public class ClientUiApplication {
+public class ZuulServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ClientUiApplication.class, args);
+		SpringApplication.run(ZuulServerApplication.class, args);
 	}
 
 }
